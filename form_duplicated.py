a851c699af6a7e12bd040905ee853535da855dd6
Rushikesh Salunke <32097552+rushikesh-salunke@users.noreply.github.com>
2019-07-10 12:17:07 +0530

    Update form_duplicated.py

--- form_duplicated.py
+++ form_duplicated.py
@@ -8,3 +8,17 @@ class ContactForm(FlaskForm):
     surname = StringField('Surname', validators=[Length(min=-1, max=100, message='You cannot have more than 100 characters')])
     email = StringField('E-Mail', validators=[Email(), Length(min=-1, max=200, message='You cannot have more than 200 characters')])
     phone = StringField('Phone', validators=[Length(min=-1, max=20, message='You cannot have more than 20 characters')])
+    
+    class ContactForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired(), Length(min=-1, max=80, message='You cannot have more than 80 characters')])
+    surname = StringField('Surname', validators=[Length(min=-1, max=100, message='You cannot have more than 100 characters')])
+    email = StringField('E-Mail', validators=[Email(), Length(min=-1, max=200, message='You cannot have more than 200 characters')])
+    phone = StringField('Phone', validators=[Length(min=-1, max=20, message='You cannot have more than 20 characters')])
+    
+    class ContactForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired(), Length(min=-1, max=80, message='You cannot have more than 80 characters')])
+    surname = StringField('Surname', validators=[Length(min=-1, max=100, message='You cannot have more than 100 characters')])
+    email = StringField('E-Mail', validators=[Email(), Length(min=-1, max=200, message='You cannot have more than 200 characters')])
+    phone = StringField('Phone', validators=[Length(min=-1, max=20, message='You cannot have more than 20 characters')])
+
+ 
