200ce028a040dffb47bdb00cb3864e5ed9a94b30
Rushikesh Salunke <32097552+rushikesh-salunke@users.noreply.github.com>
2019-07-10 12:26:17 +0530

    Update models.py

--- models.py
+++ models.py
@@ -43,3 +43,27 @@ class Contact(db.Model):
 
     def __repr__(self):
         return '<Contacts %r>' % self.name
+##
+#test
+
+from models import db, Contact
+from faker import Factory
+
+fake = Factory.create()
+# Spanish
+#fake = Factory.create('es_ES')
+# Reload tables
+db.drop_all()
+db.create_all()
+# Make 100 fake contacts
+for num in range(100):
+    fullname = fake.name().split()
+    name = fullname[0]
+    surname = ' '.join(fullname[1:])
+    email = fake.email()
+    phone = fake.phone_number()
+    # Save in database
+    mi_contacto = Contact(name=name, surname=surname, email=email, phone=phone)
+    db.session.add(mi_contacto)
+
+db.session.commit()
